0c999058e8ec52e8ccaf91952be9e3db47f3a1a0
kakegawa <cmuwni@gmail.com>
2019-08-04 21:03:59 +0900

    投稿一覧画面を作成

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -1,2 +1,5 @@
 class PostingsController < ApplicationController
+  def index
+    @postings = Posting.all
+  end
 end
---
+++ db/migrate/20190804112814_create_postings.rb
@@ -0,0 +1,7 @@
+class CreatePostings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :postings do |t|
+      t.text :content
+    end
+  end
+end
