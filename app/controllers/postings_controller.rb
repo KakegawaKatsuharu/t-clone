62d5772eeea4e7c8c760033ad7472a6cc6c0f072
kakegawa <cmuwni@gmail.com>
2019-08-07 21:42:27 +0900

    投稿削除機能追加

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -1,4 +1,6 @@
 class PostingsController < ApplicationController
+  before_action :set_posting, only:[:show, :edit, :update, :destroy]
+
   def index
     @postings = Posting.all
   end
@@ -17,20 +19,32 @@ class PostingsController < ApplicationController
   end
 
   def show
-    @posting = Posting.find(params[:id])
+    set_posting
   end
 
   def edit
-    @posting = Posting.find(params[:id])
+    set_posting
   end
 
   def update
-    @posting = Posting.find(params[:id])
+    set_posting
       if @posting.update(content: params[:posting][:content])
         redirect_to postings_path, notice: "投稿を編集しました！"
       else
         render :edit
       end
+  end
+
+  def destroy
+    set_posting.destroy
+    redirect_to postings_path, notice:"投稿を削除しました！"
+  end
+
+
+  private
+
+  def set_posting
+    @posting = Posting.find(params[:id])
   end
 
 end
